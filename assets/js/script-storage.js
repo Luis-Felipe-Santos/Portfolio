71f941732cf304f76fb2f8fef38b11166f7cfe3e
Luis-Felipe-Santos <luysfelyp07@gmail.com>
2022-12-11 01:24:50 -0300

    Desafio #2 - Dados do form no localStorage

--- assets/js/script-storage.js
+++ assets/js/script-storage.js
@@ -1,21 +1,53 @@
-var contForm = document.getElementById("form");
+//Recebe o seletor do formulário
+    const formCont = document.getElementById("form");
+    const enviarBtn = document.getElementById("enviar"); 
+    const cancelarBtn = document.getElementById("cancelar"); 
+    const modalCont = document.getElementById("modalMensagens");
+    const limparBtn = document.getElementById("clear"); 
 
-contForm.addEventListener("submit", (e) => {
+    formCont.addEventListener("submit", (e) => {
+    //Não recarrega a página
     e.preventDefault();
-    var nome_usuario = document.getElementById("nome_usuario").value;
-    var email_usuario = document.getElementById("email_usuario").value;
-    var telefone_usuario = document.getElementById("telefone_usuario").value;
-    var mensagem_usuario = document.getElementById("mensagem_usuario").value;
+    });
 
-    let usuarios = new Array();
+    enviarBtn.addEventListener('click', (e) => {
+        var nome_usuario = document.getElementById("nome_usuario").value;
+        var email_usuario = document.getElementById("email_usuario").value;
+        var telefone_usuario = document.getElementById("telefone_usuario").value;
+        var mensagem_usuario = document.getElementById("mensagem_usuario").value;
 
-    if(localStorage.hasOwnProperty("usuarios")){
-        usuarios = JSON.parse(localStorage.getItem("usuarios"));
-    }
-
-    usuarios.push({nome_usuario, email_usuario, telefone_usuario, mensagem_usuario});
-
-    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+        //Cria o array de objetos
+        let contatos = new Array();
     
-});
+        //Verifica se existe a proriedade no localStorage
+        if(localStorage.hasOwnProperty("contatos")){
+        //Converte string em objeto
+        contatos = JSON.parse(localStorage.getItem("contatos"));
+        }
+        
+        //Adiciona um novo objeto no array criado
+        contatos.push({nome_usuario, email_usuario, telefone_usuario, mensagem_usuario});
+    
+        //Salva os dados no localStorage e converte o objeto em string
+        localStorage.setItem("contatos", JSON.stringify(contatos));
+        
+        //Recupera os dados salvados no localStorage e exibe no modal
+        document.getElementById("mensagens").insertAdjacentHTML('beforeend', "Nome: " + nome_usuario + "<br>E-mail: " + email_usuario + "<br> Telefone: " + telefone_usuario + "<br> Mensagem: " + mensagem_usuario + "<br><hr>"); 
+    });
+
+    cancelarBtn.addEventListener('click', (e) => {
+        nome_usuario.value = '';
+        email_usuario.value = '';
+        telefone_usuario.value ='';
+        mensagem_usuario.value = '';
+    });
+
+    limparBtn.addEventListener('click', (e) => {
+        localStorage.clear()
+    
+    });
+
+    
+
+
 
